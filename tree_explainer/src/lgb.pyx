88e1b30ae9a562fc38b56eb1c8bf22befed15ab1
samet çopur <sametcopur@yahoo.com>
2024-10-08 02:32:33 +0300

    update

--- tree_explainer/src/lgb.pyx
+++ tree_explainer/src/lgb.pyx
@@ -11,16 +11,6 @@ from libcpp.pair cimport pair
 ctypedef pair[double, double] RangePair
 
 from cython cimport boundscheck, wraparound, initializedcheck, nonecheck, cdivision, overflowcheck, infer_types
-
-cdef class Node:
-    cdef str node_index
-    cdef str left_child
-    cdef str right_child
-    cdef int split_feature
-    cdef double threshold
-    cdef double value
-    cdef str parent_index
-
 @boundscheck(False)
 @nonecheck(False)
 @wraparound(False)
@@ -28,16 +18,15 @@ cdef class Node:
 @cdivision(True)
 @overflowcheck(False)
 @infer_types(False)
-cdef void traverse_lightgbm(dict node_dict, str node_index, vector[RangePair]& feature_ranges, vector[Rule]& rules, int tree_index):
-    cdef Node node = node_dict[node_index]
+cdef void traverse_lightgbm_tree(dict node, vector[RangePair]& feature_ranges, vector[Rule]& rules, int tree_index):
     cdef int int_node_index, feature_index
     cdef Rule rule
     cdef double lb, ub, threshold
     cdef RangePair prev_range
 
-    if node.split_feature == -1:
+    if "leaf_index" in node:
         # Leaf node
-        int_node_index = int(node_index[node_index.find("L") + 1:])
+        int_node_index = int(node["leaf_index"])
         rule = create_rule(len(feature_ranges), tree_index, int_node_index)
 
         # Update rule with feature ranges
@@ -46,30 +35,31 @@ cdef void traverse_lightgbm(dict node_dict, str node_index, vector[RangePair]& f
             if lb != -INFINITY or ub != INFINITY:
                 update_rule(&rule, feature_index, lb, ub)
 
-        rule.value = node.value
+        rule.value = node["leaf_value"]
         rules.push_back(rule)
         return
 
     # Split node
-    feature_index = node.split_feature
-    threshold = node.threshold
+    feature_index = int(node["split_feature"])
+    threshold = node["threshold"]
 
     # Save previous range
     prev_range = feature_ranges[feature_index]
 
     # Update feature range for left child
     feature_ranges[feature_index] = (feature_ranges[feature_index].first, threshold)
-    traverse_lightgbm(node_dict, node.left_child, feature_ranges, rules, tree_index)
+    traverse_lightgbm_tree(node["left_child"], feature_ranges, rules, tree_index)
 
     # Restore previous range
     feature_ranges[feature_index] = prev_range
 
     # Update feature range for right child
     feature_ranges[feature_index] = (threshold, feature_ranges[feature_index].second)
-    traverse_lightgbm(node_dict, node.right_child, feature_ranges, rules, tree_index)
+    traverse_lightgbm_tree(node["right_child"], feature_ranges, rules, tree_index)
 
     # Restore previous range
     feature_ranges[feature_index] = prev_range
+
 
 @boundscheck(False)
 @nonecheck(False)
@@ -79,47 +69,18 @@ cdef void traverse_lightgbm(dict node_dict, str node_index, vector[RangePair]& f
 @overflowcheck(False)
 @infer_types(False)
 cdef vector[vector[Rule]] analyze_lightgbm(object model, int len_col):
-    cdef object trees_df, tree_df
     cdef int tree_index
-    cdef str root_index
     cdef Rule rule
     cdef vector[vector[Rule]] trees = vector[vector[Rule]]()
     cdef vector[Rule] rules
-    cdef cnp.ndarray[cnp.int64_t, ndim=1] tree_indices
-    cdef dict node_dict
-    cdef Node node, root_node
+    cdef dict node
     cdef vector[RangePair] feature_ranges
 
-    trees_df = model.trees_to_dataframe()
-    tree_indices = trees_df.tree_index.unique()
+    # Use tree_list instead of trees_df
+    cdef list tree_list = model.dump_model()["tree_info"]
 
-    for tree_index in tree_indices:
-        tree_df = trees_df[trees_df["tree_index"] == tree_index]
-
-        # Build node_dict
-        node_dict = {}
-        for idx in tree_df.index:
-            row = tree_df.loc[idx]
-            node = Node()
-            node.node_index = row["node_index"]
-            node.left_child = row["left_child"]
-            node.right_child = row["right_child"]
-            node.parent_index = row["parent_index"]
-
-            split_feature = row["split_feature"]
-            if pd.isna(split_feature):
-                node.split_feature = -1  # Leaf node
-            else:
-                node.split_feature = int(split_feature.replace("Column_", ""))
-            node.threshold = row["threshold"]
-            node.value = row["value"]
-            node_dict[node.node_index] = node
-
-        # Find root node
-        for node in node_dict.values():
-            if pd.isna(node.parent_index):
-                root_node = node
-                break
+    for tree_index in range(len(tree_list)):
+        node = tree_list[tree_index]["tree_structure"]
 
         # Initialize feature_ranges
         feature_ranges = vector[RangePair](len_col)
@@ -129,8 +90,8 @@ cdef vector[vector[Rule]] analyze_lightgbm(object model, int len_col):
         # Initialize rules vector
         rules = vector[Rule]()
 
-        # Traverse tree
-        traverse_lightgbm(node_dict, root_node.node_index, feature_ranges, rules, tree_index)
+        # Traverse the tree structure
+        traverse_lightgbm_tree(node, feature_ranges, rules, tree_index)
 
         # Sort rules
         sort(rules.begin(), rules.end(), compare_rules)
